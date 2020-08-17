7cb278e14205d8a8e1bd473b3921c23956b11c41
Andre Santos <japansantos@gmail.com>
2020-08-17 11:02:42 -0300

    Criei o arquivo andre01.py

---
+++ andre01.py
@@ -0,0 +1,4 @@
+dia = 17
+mes = 8
+ano = 2020
+print(dia, '/', mes, '/', ano)
---
+++ andre04.py
@@ -0,0 +1,5 @@
+h = int(input('digite as horas'))
+m = int(input('digite os minutos'))
+s = int(input('digite os segundos'))
+r = h*3600+m*60+s
+print( r )
