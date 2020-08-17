a8d9e59818cb4b06f2df2af125fb6de09145b814
Andre Santos <69485599+186andresantos@users.noreply.github.com>
2020-08-17 12:19:03 -0300

    Add files via upload

---
+++ andre05.py
@@ -0,0 +1,16 @@
+larg = float(input('largura da sala'))
+compri = float(input('comprimento da sala'))
+a = larg*compri
+print((a), 'metros')
+larg = float(input('largura da sala'))
+compri = float(input('comprimento da sala'))
+alt = float(input('altura da sala'))
+volume = (larg*compri*alt)**3
+print( volume, 'metros')
+altura = float(input('altura da parede'))
+largura = float(input('largura da parede'))
+comprimento = float(input('comprimento da parade'))
+area = (2*(altura*largura)+2)*altura*comprimento
+print( area, 'metros')
+tinta = area/2
+print(tinta, 'litros')
